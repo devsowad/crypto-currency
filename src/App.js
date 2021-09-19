9e0af18c712a24d6524b743260358d1e0248653e
Sowad <sowad1035@gmail.com>
2021-09-19 16:56:44 +0600

    Setup project, tailwindcss, fontawesome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { faReact } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='dark'>
+      <div className='h-screen flex items-center justify-center text-center  dark:bg-gray-700 bg-purple-200'>
+        <div>
+          <p className='dark:text-white text-4xl'>Hello React!</p>
+          <FontAwesomeIcon
+            className='mt-4 text-4xl text-dark-600 dark:text-blue-400'
+            icon={faReact}
+            size='lg'
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
